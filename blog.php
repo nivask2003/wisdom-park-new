008b21d78622d2fae6b61c84935f7a6ef8623d68
Nivas Kumar S <nivask457@gmail.com>
2026-07-15 10:39:59 +0530

    File Updated

---
+++ blog.php
@@ -0,0 +1,37 @@
+<?php include "function.php" ?>
+<html lang="en">
+
+<head>
+	<title>Wisdom Park International Schoool (CBSE)</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link id="favicon" rel="shortcut icon" type="image/png" href="images/fav-icon.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/header-reponsive.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+</head>
+
+<body>
+	<?=template_header()?>
+	<main>
+		<section class="blog-1">
+			<div class="container">
+				<div class="heading text-center">
+					<h6 class="sub-title">Blogs</h6>
+					<h1 class="title">Insights, Stories & School News</h1>
+					<p class="desc">Stay updated with our latest school news, student achievements, inspiring stories,
+						educational events,<br>and activities that shape every learner's journey.</p>
+				</div>
+			</div>
+		</section>
+	</main>
+	<?=template_footer()?>
+</body>
+
+</html>
